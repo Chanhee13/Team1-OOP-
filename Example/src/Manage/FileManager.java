9c47530df832f9148079e0f796ae648448584b08
Key <kyy8383@naver.com>
2017-05-23 17:29:09 +0900

    f

--- Example/src/Manage/FileManager.java
+++ Example/src/Manage/FileManager.java
@@ -1,24 +1,26 @@
 package Manage;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import User.Hero;
 import User.User;
 
-public class FileManager{
-	
-	private File[] file= new File[3];
+public class FileManager {
+
+	private File[] file = new File[3];
 	private User user = new User();
 	private ArrayList<File> f = new ArrayList<File>();
-	
+
 	public void CreateFile() // 만드는건 void라 판단한거 같아서 일단 void로 해두었어요.
 	{
-		//Test 용으로 설정 해두었습니다.
-		for(int i=0;i<3;i++){
-			
-			this.file[i] = new File("Test"+i+".txt"); // 이 부분은 텍스트파일 생성인데 안보셔도 됩니다.
+		// Test 용으로 설정 해두었습니다.
+		for (int i = 0; i < 3; i++) {
+
+			this.file[i] = new File("Test" + i + ".txt"); // 이 부분은 텍스트파일 생성인데
+															// 안보셔도 됩니다.
 			try {
 				file[i].createNewFile();
 				file[i].mkdirs();
@@ -28,42 +30,39 @@ public class FileManager{
 			}
 		}
 	}
-	
-	public void MakeID(String ID)
-	{
-		
-	}
-	
-	public User makeUser(){
-		
+
+	public void MakeID(String ID) throws IOException {
+		FileWriter fw = new FileWriter("text1.txt"); // 일단 text1에 저장해 두었음
 		User user = new User();
-		//나중에는 Scanner나 GUI의 함수로 값을 받아오겠지만
-		//지금은 그냥 직접 값을 넣도록 한다.
+		String data = user.getID(); //받은 것을 저장하는 것인지 파일에서 받아서 저장하는 것인지모르겠음 일단 받은 것을 저장하는 것으로 해놓았음
+		fw.write(data);
+		fw.close();
+	}
+
+	public User makeUser() {
+
+		User user = new User();
+		// 나중에는 Scanner나 GUI의 함수로 값을 받아오겠지만
+		// 지금은 그냥 직접 값을 넣도록 한다.
 		user.setID("temp");
 		user.setHero(new Hero());
-		boolean[] cleard = {true, false, false, false, false, false, false, false};
+		boolean[] cleard = { true, false, false, false, false, false, false, false };
 		user.setCleared(cleard);
-		
 		return user;
 	}
-	
-	public void addFile()
-	{
-		for(int i=0;i<3;i++)
-		{
+
+	public void addFile() {
+		for (int i = 0; i < 3; i++) {
 			f.add(this.file[i]);
 		}
 	}
-	
-	public void removeFile()
-	{
+
+	public void removeFile() {
 		f.remove(this.file[0]);
 	}
-	
-	public void loadFile()
-	{
-		System.out.println("Test loading : "+this.f);
+
+	public void loadFile() {
+		System.out.println("Test loading : " + this.f);
 	}
-	
 
 }
--- Example/src/game/Stage.java
+++ Example/src/game/Stage.java
@@ -3,7 +3,20 @@ package game;
 import Monster.Monster;
 
 public class Stage {
-	
+	private int StageNumber;
 	Monster[] monster = new Monster[24];
+
+public void nextMonster()//다음 스테이지로 넘어가는 거 
+{
+	
+}
+
+public int getStageNumber() {
+	return StageNumber;
+}
+
+public void setStageNumber(int stageNumber) {
+	StageNumber = stageNumber;
+}
 
 }
