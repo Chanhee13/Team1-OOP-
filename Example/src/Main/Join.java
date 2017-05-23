daacb79477938fba158d536bec074af7766f551a
Key <kyy8383@naver.com>
2017-05-23 23:08:24 +0900

    main

---
+++ Example/src/Main/Join.java
@@ -0,0 +1,201 @@
+package Main;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+public class Join extends JFrame implements ActionListener {
+
+	private String id;
+	private String password;
+	private String age;
+	public JLabel whatid;
+	public JLabel whatpassword;
+	public JLabel whatage;
+	public JLabel sameid;
+	public JTextField writenewusername;
+	public JTextField writenewpassword;
+	public JTextField writeage;
+	public JButton saveid;
+	public JButton cancel;
+	JFrame frame = new JFrame();
+	
+	public void Joinpress() // 회원가입을 누르면 이 클래스가 나온다
+	{
+		frame.setTitle("Create ID");
+		Container panel = frame.getContentPane();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints cs = new GridBagConstraints();
+		cs.fill = GridBagConstraints.HORIZONTAL;
+
+		TitledBorder border = BorderFactory.createTitledBorder("making id for the game");
+		((JComponent) panel).setBorder(border); // 위에 들어가는것 title
+
+		whatid = new JLabel("Userid: ");
+		cs.gridx = 0;
+		cs.gridy = 0;
+		cs.gridwidth = 1;
+		panel.add(whatid, cs); // 아이디를 쓰시오 ( Id :)
+
+		writenewusername = new JTextField(20);
+		cs.gridx = 1;
+		cs.gridy = 0;
+		cs.gridwidth = 2;
+		panel.add(writenewusername, cs); // 아이디 쓰는 칸
+
+		sameid = new JLabel();
+		cs.gridx = 2;
+		cs.gridy = 0;
+		cs.gridwidth = 1;
+		panel.add(sameid, cs);
+
+		whatpassword = new JLabel("Password: ");
+		cs.gridx = 0;
+		cs.gridy = 1;
+		cs.gridwidth = 1;
+		panel.add(whatpassword, cs); // 패스워드를 쓰시오 (Password :)
+
+		writenewpassword = new JTextField(20);
+		cs.gridx = 1;
+		cs.gridy = 1;
+		cs.gridwidth = 2;
+		panel.add(writenewpassword, cs); // 패스워드 쓰는 칸
+
+		whatage = new JLabel("Age: (number)"); // 나이:
+		cs.gridx = 0;
+		cs.gridy = 2;
+		cs.gridwidth = 1;
+		panel.add(whatage, cs);
+
+		writeage = new JTextField(20); // 나이 쓰는칸
+		cs.gridx = 1;
+		cs.gridy = 2;
+		cs.gridwidth = 2;
+		panel.add(writeage, cs);
+
+		saveid = new JButton("save the information"); // 회원가입 확인 !
+		cs.gridx = 0;
+		cs.gridy = 3;
+		cs.gridwidth = 2;
+		panel.add(saveid, cs);
+
+		cancel = new JButton("Canel");
+		cs.gridx = 2;
+		cs.gridy = 3;
+		cs.gridwidth = 1;
+		panel.add(cancel, cs);
+
+		saveid.addActionListener(this); // 회원가입 확인만 actionlistener을 넣으면 된다
+		cancel.addActionListener(this);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(saveid)) {
+			
+			char token = ',';
+			Join a = new Join();
+			a.setId(writenewusername.getText());
+			a.setPassword(writenewpassword.getText()); // 임의로 하나의 join을 만들어서 입력한  것을 저장한다
+			a.setAge(writeage.getText());
+			String[][] indat = new String[100][5]; // indat에 csv 하나하나를 넣는다.
+			int row = 0, i;
+			
+			
+			try {
+				File path = new File("");
+			    File csv = new File(path.getCanonicalPath()+"//join.csv");
+				
+			   BufferedReader br = new BufferedReader(new FileReader(csv));
+				String line = "";
+				while ((line = br.readLine()) != null) {
+					String[] token1 = line.split(",", -1);
+					for (i = 0; i < 5; i++) {
+						indat[row][i] = token1[i];
+							if (a.id.equals(indat[row][0])) // 사용자가 id를 친것과 csv의 아이디 칸에 같은것이있을때
+							{
+									idcheck idc = new idcheck();
+									indat= null;
+									br.close();
+									return;
+							}
+						}
+					row++;
+					}
+				br.close();
+				
+				BufferedWriter bw = new BufferedWriter(new FileWriter("join.csv"));
+				indat[row][0] = a.getId();
+				indat[row][1] = a.getPassword();
+				indat[row][2] = a.getAge();
+				indat[row][3] = "0";
+				indat[row][4] = "0";
+				for (i = 0; i <= row; i++) {
+					bw.write(indat[i][0] + token + indat[i][1] + token + indat[i][2] + token + indat[i][3]+token +indat[i][4]);
+					bw.write("\n");
+				}
+				bw.close();
+				
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			
+			frame.dispose();
+		}else if(e.getSource().equals(cancel))
+
+	{
+		frame.dispose();
+	}
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String string) {
+		this.age = string;
+	}
+
+}
---
+++ Example/src/Main/Main.java
@@ -0,0 +1,161 @@
+package Main;
+
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+public class Main extends JFrame implements ActionListener {
+
+	public JLabel Username;
+	public JLabel Password;
+	public JTextField writeUsername;
+	public JTextField writePassword;
+	public JButton okay;
+	public JButton joinid;
+	JFrame frame = new JFrame();
+	public static String ID;
+
+	public Main() {
+
+		frame.setTitle("JavaProgramGame");
+		Container panel = frame.getContentPane();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints cs = new GridBagConstraints();
+		cs.fill = GridBagConstraints.HORIZONTAL;
+
+		TitledBorder border = BorderFactory.createTitledBorder("Login to start the game");
+		((JComponent) panel).setBorder(border);
+
+		Username = new JLabel("Userid: ");
+		cs.gridx = 0;
+		cs.gridy = 0;
+		cs.gridwidth = 1;
+		panel.add(Username, cs); // 아이디를 쓰시오 ( Id :)
+
+		writeUsername = new JTextField(20);
+		cs.gridx = 1;
+		cs.gridy = 0;
+		cs.gridwidth = 2;
+		panel.add(writeUsername, cs); // 아이디 쓰는 칸
+
+		Password = new JLabel("Password: ");
+		cs.gridx = 0;
+		cs.gridy = 1;
+		cs.gridwidth = 1;
+		panel.add(Password, cs); // 패스워드를 쓰시오 (Password :)
+
+		writePassword = new JTextField(20);
+		cs.gridx = 1;
+		cs.gridy = 1;
+		cs.gridwidth = 2;
+		panel.add(writePassword, cs); // 패스워드 쓰는 칸
+
+		okay = new JButton("Login");
+		cs.gridx = 0;
+		cs.gridy = 2;
+		cs.gridwidth = 2;
+		panel.add(okay, cs);
+
+		joinid = new JButton("Sign Up");
+		cs.gridx = 1;
+		cs.gridy = 2;
+		cs.gridwidth = 2;
+		panel.add(joinid, cs);
+
+		okay.addActionListener(this);
+		joinid.addActionListener(this);
+
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+
+		Main a = new Main();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String[][] indat = new String[100][5]; // indat에 csv 하나하나를 넣는다.
+		int row = 0, i;
+				
+		if (e.getSource().equals(joinid)) // 회원가입을 눌렀을때
+		{
+			Join a = new Join();
+			a.Joinpress();
+		}
+
+		else if (e.getSource().equals(okay)) // 확인 버튼을 눌렀을때
+		{
+			setID(writeUsername.getText());
+			try {
+				File path = new File("");
+			    File csv = new File(path.getCanonicalPath()+"//join.csv");
+				BufferedReader br = new BufferedReader(new FileReader(csv));
+				
+				String line = "";
+				while ((line = br.readLine()) != null) {
+					String[] token = line.split(",");
+					for (i = 0; i < 5; i++) {
+						indat[row][i] = token[i];
+					}
+					row++;
+				}
+				br.close();
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			for ( i = 0; i < 100; i++) {
+				if (writeUsername.getText().equals(indat[i][0])) // 사용자가 id를 친것과 csv의 아이디 칸에 같은것이있을때
+				{
+					if (writePassword.getText().equals(indat[i][1])) // 사용자의 id가 같고  password를 csv와 비교할때
+					{
+				//		GameorStudy a1 = new GameorStudy(); //게임으로 들어가야 하는것인데 음 우리 클래스에서 어디로 들어가는것인지 모르겠음
+						
+						frame.dispose();
+					}
+
+				}
+
+			}
+			if (frame.isShowing() == true) // 만약 이 class 의 frame이 살아있다면!!
+			{
+				IdPasswordCheck a = new IdPasswordCheck();
+				frame.dispose();
+			} // 틀리면 IdPasswordCheck 클래스로가서 다시 Idmake JFrame으로 옮겨진다
+
+		}
+
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+}
