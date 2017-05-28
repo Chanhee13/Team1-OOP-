b10853ce55fdeaf6e01dfc22beb36cfd870577ce
skysa <skysa@DESKTOP-7KI1PCH>
2017-05-28 21:06:56 +0900

    재시도

--- Example/src/game/Stage_GUI.java
+++ Example/src/game/Stage_GUI.java
@@ -10,9 +10,10 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
+
 
 import Monster.Monster;
 
