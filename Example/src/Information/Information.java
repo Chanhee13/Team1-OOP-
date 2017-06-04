9b4368a7f372fed8b87a08180d3ed17b64f34a6b
Key <kyy8383@naver.com>
2017-06-04 23:29:46 +0900

    무시하세염

--- Example/src/Information/Information.java
+++ Example/src/Information/Information.java
@@ -13,81 +13,73 @@ import java.util.ArrayList;
 import Dr.Dr_T;
 
 public class Information implements Serializable {
-	private String ID;
-	private String toTeach;
-	private ArrayList<Assignment> assingments = new ArrayList<Assignment>();
-	
-	public Information(int stage){
-		String f = "stage"+stage+".txt";
-		try {
-			File file = new File(f);
-			FileReader read = new FileReader(file);
-			BufferedReader br = new BufferedReader(read);
-			
-			String text;
-			String copytext="";
-			
-			while((text = br.readLine())!=null)
-			{
-				copytext+=text;
-				copytext+="\n";
-			}
-			//스테이지 지정
-			String[] result = copytext.split("&");
-			this.setID(result[0]);
-			//가르칠 내용 지정
-			this.setToTeach(result[1]);
-			Dr_T.setToTeach(result[1]);
-			String[] preAssingments = result[2].split(".");
-			//문제와 정답과 오답 설정
-			for(String s : preAssingments){
-				String[] tempStr = s.split(",");
-				if(tempStr[0]=="ox"){
-					boolean sol=true;
-					if(tempStr[2]=="X"){
-						sol=false;
-					}
-					assingments.add(new OXAssignment(Integer.parseInt(this.getID()),tempStr[1],sol));
-				}
-				if(tempStr[0]=="se"){
-					String[] wro = {tempStr[3],tempStr[4],tempStr[5]};
-					assingments.add(new SelectAssignment(Integer.parseInt(this.getID()),tempStr[1],tempStr[2],wro));
-				}
-				if(tempStr[0]=="ty"){
-					assingments.add(new TypeAssignment(Integer.parseInt(this.getID()),tempStr[1],tempStr[2]));
-				}
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+   private String ID;
+   private String toTeach;
+   private static ArrayList<Assignment> assingments = new ArrayList<Assignment>();
+   
+   public Information(int stage){
+      String f = "stage"+stage+".txt";
+      try {
+         File file = new File(f);
+         FileReader read = new FileReader(file);
+         BufferedReader br = new BufferedReader(read);
+      
+         String text;
+         String copytext="";
+         String[] result = new String[100];
+         while((text = br.readLine())!=null)
+         {
+            copytext +=text;
+            copytext +="\n";
+         }
+         
+         result = copytext.split("&");
+         //스테이지 지정
+      
+         System.out.println(result[1]);
+         result[0].trim();
+         setID(result[0]);
+         //가르칠 내용 지정
+         setToTeach(result[1]);
+         Dr_T.setToTeach(result[1]);
+         String[] preAssingments = result[2].split(".");
+         //문제와 정답과 오답 설정
+         String[] tempStr = result[2].split(",");
+         /*이부분은 for문을 통해서 addAssignment해야 되는 부분인데 for문이 돌지 않을 뿐더러
+          * 오류가 있어서 잠시 보류중입니다.
+          */
+      
+      } catch (FileNotFoundException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      } catch (IOException e)
+      {
+         e.printStackTrace();
+      }
 
-	}
-	public String getID() {
-		return ID;
-	}
-	public void setID(String iD) {
-		ID = iD;
-	}
-	
-	public String getToTeach() {
-		return toTeach;
-	}
-	public void setToTeach(String toTeach) {
-		this.toTeach = toTeach;
-	}
-	
-	//assingment에 값을 넣거나 빼고, 조회할 때는 ArrayList의 method인 add, remove를 쓰도록 한다. 
-	//하지만   다른 클래스에서 assingment 전체의 정보를 얻어오고 싶은 경우에는 getAssingment를 사용한다.
-	public ArrayList<Assignment> getAssingments() {
-		return assingments;
-	}
-	
-	public void setAssingments(ArrayList<Assignment> assingments) {
-		this.assingments = assingments;
-	}
+   }
+   public String getID() {
+      return ID;
+   }
+   public void setID(String iD) {
+      ID = iD;
+   }
+   
+   public String getToTeach() {
+      return toTeach;
+   }
+   public void setToTeach(String toTeach) {
+      this.toTeach = toTeach;
+   }
+   
+   //assingment에 값을 넣거나 빼고, 조회할 때는 ArrayList의 method인 add, remove를 쓰도록 한다. 
+   //하지만   다른 클래스에서 assingment 전체의 정보를 얻어오고 싶은 경우에는 getAssingment를 사용한다.
+   public ArrayList<Assignment> getAssingments() {
+      return assingments;
+   }
+   
+   public void setAssingments(ArrayList<Assignment> assingments) {
+      this.assingments = assingments;
+   }
 
 }
